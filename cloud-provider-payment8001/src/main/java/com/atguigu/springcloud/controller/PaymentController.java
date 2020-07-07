9097a6c73e72b784e5d761124b25f5bac1f1c70a
1003082940@qq.com <Dzyu@123!>
2020-07-08 02:14:02 +0800

    message:spring-cloud,openfeign的使用和超时连接处理，openfeign也默认整合了ribbon的负载均衡 committer:nirvanaYDZ branches:master

--- cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @Slf4j
@@ -64,6 +65,17 @@ public class PaymentController {
 
     @GetMapping(value = "/payment/lb")
     public String getServerPort(){
+        return serverPort;
+    }
+
+    // 模拟服务超时调用场景
+    @GetMapping(value = "/payment/feign/timeout")
+    public String paymentFeignTimeout(){
+        try {
+            TimeUnit.SECONDS.sleep(3);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         return serverPort;
     }
 }
