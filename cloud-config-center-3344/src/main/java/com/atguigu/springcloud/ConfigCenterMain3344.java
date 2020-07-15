6903816db23b4cee7f69c3ae0f52d3574b4d58bc
1003082940@qq.com <Dzyu@123!>
2020-07-15 12:28:30 +0800

    message:spring-cloud,spring-cloud配置引入消息总线，向客户端定向发送配置文件的更新，需要搭建rabbitmq服务端 committer:nirvanaYDZ branches:master

--- cloud-config-center-3344/src/main/java/com/atguigu/springcloud/ConfigCenterMain3344.java
+++ cloud-config-center-3344/src/main/java/com/atguigu/springcloud/ConfigCenterMain3344.java
@@ -7,6 +7,9 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @SpringBootApplication
 @EnableConfigServer
 public class ConfigCenterMain3344 {
+
+    // 开启总线配置后发送curl请求 curl -X POST "http://localhost:3344/actuator/bus-refresh"
+    // 当只想通知某一个服务端时 curl -X POST "http://localhost:3344/actuator/bus-refresh/config-client:3355"
     public static void main(String[] args) {
         SpringApplication.run(ConfigCenterMain3344.class,args);
     }
--- cloud-config-client-3366/src/main/java/com/atguigu/springcloud/controller/ConfigClientController.java
+++ cloud-config-client-3366/src/main/java/com/atguigu/springcloud/controller/ConfigClientController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RefreshScope // 自动化刷新标签，且需要发送post请求给3355，刷新 curl -X POST "http://localhost:3355/actuator/refresh"
+@RefreshScope
 public class ConfigClientController {
 
     @Value("${server.port}")
